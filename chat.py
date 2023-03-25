6b4a550efed22e41795f004b609912c6a93d83d3
SaturnB001 <128606217+SaturnB001@users.noreply.github.com>
2023-03-24 21:45:09 -0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,37 +1,27 @@
-import openai
-from prompt_toolkit import prompt
+import googlesearch
+import urllib.request
+from bs4 import BeautifulSoup
 
-# Defina sua chave de API da OpenAI
-openai.api_key = "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"
+print("Olá, eu sou um chatbot! O que você gostaria de saber?")
 
-# Defina o modelo que você deseja usar (pode ser "text-davinci-002" para uma versão mais avançada)
-model_engine = "text-davinci-001"
-
-# Inicialize o modelo
-def get_model():
-    return openai.Model(engine=model_engine)
-
-# Função principal que roda o ChatBot
-def chat(prompt_text, model, length=50):
-    response = model.generate(
-        prompt=prompt_text,
-        max_length=length,
-        temperature=0.7,
-        n=1,
-        stop=None,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    message = response.choices[0].text.strip()
-    return message
-
-# Loop principal para continuar a conversa
 while True:
-    # Obtém a entrada do usuário
-    user_input = prompt("Você: ")
-    
-    # Obter a resposta do modelo GPT-2
-    bot_response = chat(user_input, get_model())
-    
-    # Imprime a resposta do ChatBot
-    print("ChatBot: " + bot_response)
+    # Recebe a pergunta do usuário
+    pergunta = input("> ")
+
+    # Usa o módulo googlesearch para buscar a resposta na web
+    resultados = googlesearch.search(pergunta, num_results=3)
+
+    # Acessa o primeiro link encontrado e extrai o texto da página
+    resposta = ""
+    for link in resultados:
+        try:
+            html = urllib.request.urlopen(link).read()
+            soup = BeautifulSoup(html, 'html.parser')
+            texto = soup.get_text()
+            resposta += texto
+            break
+        except:
+            pass
+
+    # Imprime a resposta encontrada
+    print(resposta)
